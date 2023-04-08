722a98322102e0eca2087891af1dbd0f540cee76
ahmedgamal115 <ahmedgamal1153gmail.com>
2023-04-08 21:34:39 +0200

    Object detect tf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect,useRef } from "react";
+import '@tensorflow/tfjs-backend-webgl';
+const mobilenet = require('@tensorflow-models/mobilenet');
 
 function App() {
+  const [isModelLoading,setModelLoading] = useState(false);
+  const [model,setModel] = useState(null);
+  const [ImageURL,setImage] = useState(null);
+  const [result, setResult] = useState([]);
+  const imageRef = useRef()
+
+  const loadModel = async () =>{
+    setModelLoading(true)
+    try {
+      const mainModel = await mobilenet.load();
+      setModel(mainModel)
+      setModelLoading(false)
+    } catch (error) {
+      console.log(error)
+      setModelLoading(false)
+    }
+  }
+
+  const identify = async () => {
+    console.log(model)
+    const result = await model.classify(imageRef.current)
+    console.log(result)
+    setResult(result)
+  }
+
+  const uplaodImage = (e) => {
+    const {files} = e.target
+    if(files.length > 0)
+    {
+      const url = URL.createObjectURL(files[0])
+      setImage(url)
+    }else{
+      setImage(null)
+    }
+  }
+
+  useEffect(()=>{
+    loadModel()
+  },[])
+
+  if(isModelLoading){
+    return <h2>Model Loading........</h2>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"><h1>Image Detection</h1></div>
+      <div className="inputContiner">
+        <input type="file" accept="image/*" capture="camera" className="inputImage" onChange={uplaodImage}/>
+      </div>
+
+      <div className="mainWrapper">
+        <div className="mainContent">
+          <div className="imageHolder">
+            {ImageURL && <img className="previewImage" src={ImageURL} alt="Preview" crossOrigin="anonymous" ref={imageRef}/>}
+          </div>
+        </div>
+      </div>
+      {ImageURL && <button className="mainButton" onClick={identify}>Identify Button</button>}
+      {result.length > 0 && <div className="result">
+        {
+          result.map((results,index)=>{
+            return(
+              <div className="results" key={results.className}>
+                <span>{results.className}</span><br/>
+                <span>{(results.probability * 100).toFixed(2)} %</span>
+              </div>
+            )
+          })
+        }
+        </div>}
     </div>
   );
 }
